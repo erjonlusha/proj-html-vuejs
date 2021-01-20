52e957b02bf85eb6e2e2efbea7cfea32e2e357a5
Erjon <erjonlusha@gmail.com>
2021-01-20 15:43:03 +0100

    Completato Header

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,8 +4,13 @@
 let app = new Vue ({
 	el: "#app",
     data : {
+        search:'',
+
         menu:['home', 'pages', 'courses', 'features', 'blog', 'shop'],
-        logo:'assets/img/light-logo.png'
+        logo:'assets/img/light-logo.png',
+        screenShot:'assets/img/home-6-hero-poster-final.jpg',
+        heroLeft:'assets/img/home-6-hero-left-person.png',
+        heroRight:'assets/img/home-6-hero-right-person.png',
     },
     mounted () {},
     methods:{}
