c8ade171aafc36aaf16368af10f564e94302cfbc
Erjon <erjonlusha@gmail.com>
2021-01-20 18:11:04 +0100

    Footer completato

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,11 @@
 let app = new Vue ({
 	el: "#app",
     data : {
+        address:'382 NE 191st St # 87394 Miami, FL 33179-3899',
+        contactNumber:'+1(305) 547-9909',
+        openingHours:'9am - 5pm EST, Monday - Friday',
+        emailSupport:'support@maxcoach.com',
+        socials:['fab fa-facebook-square', 'fab fa-twitter','fab fa-instagram', 'fab fa-linkedin'],
         search:'',
 
         menu:['home', 'pages', 'courses', 'features', 'blog', 'shop'],
